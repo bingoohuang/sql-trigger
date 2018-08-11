ba6a1810838d9e51ff859d6c597073ce4dab93bd
bingoohuang <bingoo.huang@gmail.com>
2018-08-11 17:08:49 +0800

    import static

--- src/test/java/com/github/bingoohuang/sqlfilter/SqlFilterProxyTest.java
+++ src/test/java/com/github/bingoohuang/sqlfilter/SqlFilterProxyTest.java
@@ -2,7 +2,6 @@ package com.github.bingoohuang.sqlfilter;
 
 import com.github.bingoohuang.utils.lang.Mapp;
 import com.google.common.collect.Lists;
-import com.google.common.collect.Maps;
 import lombok.Cleanup;
 import lombok.SneakyThrows;
 import lombok.val;
@@ -11,6 +10,8 @@ import org.junit.Test;
 import java.util.List;
 import java.util.Map;
 
+import static com.github.bingoohuang.sqlfilter.SqlTypeTestUtil.executeSelect;
+import static com.github.bingoohuang.sqlfilter.SqlTypeTestUtil.executeUpdate;
 import static com.google.common.truth.Truth.assertThat;
 
 public class SqlFilterProxyTest {
@@ -18,13 +19,13 @@ public class SqlFilterProxyTest {
     public void test() {
         val filter = new ScheduleFilter();
         @Cleanup val conn = SqlFilterProxy.create(SqlTypeTestUtil.getH2Connection(), filter);
-        SqlTypeTestUtil.executeUpdate(conn, "delete from T_SCHEDULE");
+        executeUpdate(conn, "delete from T_SCHEDULE");
 
         {
             val sql = "insert into T_SCHEDULE(id, name, schedule_state, subscribes) values(?, ?, '正常', 9)";
             @Cleanup val stmt = conn.prepareStatement(sql);
-            SqlTypeTestUtil.executeUpdate(stmt, "1", "bingoo");
-            SqlTypeTestUtil.executeUpdate(stmt, "2", "dingoo");
+            executeUpdate(stmt, "1", "bingoo");
+            executeUpdate(stmt, "2", "dingoo");
         }
 
         assertThat(filter.getAddedSchedules()).isEqualTo(Lists.newArrayList(
@@ -32,10 +33,10 @@ public class SqlFilterProxyTest {
                 Schedule.builder().id("2").idMapped(true).name("dingoo").nameMapped(true).state("正常").stateUsed(true).subscribes(9).build()
         ));
 
-        SqlTypeTestUtil.executeUpdate(conn, "update T_SCHEDULE set name = 'bingoohuang', schedule_state = '失效' where id = '1' and name = ?", "bingoo");
+        executeUpdate(conn, "update T_SCHEDULE set name = 'bingoohuang', schedule_state = '失效' where id = '1' and name = ?", "bingoo");
         {
             @Cleanup val stmt = conn.prepareStatement("update T_SCHEDULE set name = ? where id = ?");
-            SqlTypeTestUtil.executeUpdate(stmt, "dingoohuang", "2");
+            executeUpdate(stmt, "dingoohuang", "2");
         }
 
         assertThat(filter.getUpdatedSchedules()).isEqualTo(Lists.newArrayList(
@@ -45,17 +46,17 @@ public class SqlFilterProxyTest {
                 Schedule.builder().name("dingoohuang").nameMapped(true).build()
         ));
 
-        List<Map<String, Object>> list = SqlTypeTestUtil.executeSelect(conn, "select id, name, schedule_state from T_SCHEDULE");
+        List<Map<String, Object>> list = executeSelect(conn, "select id, name, schedule_state from T_SCHEDULE");
 
         assertThat(list).isEqualTo(Lists.newArrayList(
                 Mapp.of("ID", "1", "NAME", "bingoohuang", "SCHEDULE_STATE", "失效"),
                 Mapp.of("ID", "2", "NAME", "dingoohuang", "SCHEDULE_STATE", "正常")
         ));
 
-        SqlTypeTestUtil.executeUpdate(conn, "delete from T_SCHEDULE where id = '1'");
+        executeUpdate(conn, "delete from T_SCHEDULE where id = '1'");
         {
             @Cleanup val stmt = conn.prepareStatement("delete from T_SCHEDULE where id = ?");
-            SqlTypeTestUtil.executeUpdate(stmt, "2");
+            executeUpdate(stmt, "2");
         }
 
         assertThat(filter.getDeletedSchedules()).isEqualTo(Lists.newArrayList(
